3ad04ab5390cd9dbdbbfc0b7f22c2ca1f46483ed
joll3a <its.jolla6@gmail.com>
2025-08-17 15:24:39 +0300

    v19.3

--- controllers/farmerController.js
+++ controllers/farmerController.js
@@ -82,9 +82,9 @@ exports.getAllFarmers = async (req, res) => {
 
     const total = await Farmer.countDocuments(filter);
 
-    // const farmers = await Farmer.find(filter)
-    //   .skip((page - 1) * limit)
-    //   .limit(limit);
+    const farmers = await Farmer.find(filter)
+      .skip((page - 1) * limit)
+      .limit(limit);
 
     res.json({ farmers, total });
     console.log(`Admin ID: ${adminId}, Farmers found: ${farmers.length}`)
