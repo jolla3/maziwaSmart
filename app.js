60170e9040dd6ee6f7c8d97376a4d7c7f2afc7b0
joll3a <its.jolla6@gmail.com>
2025-10-20 20:13:37 +0300

    v42.0

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ const passport = require("./config/passport");
 const app = express();
 
 // middleware
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json());
+app.use(cors());
+
 
 app.use(passport.initialize());
 
